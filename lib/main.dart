d9ccb822fffbed88a62b3d35b213d9926e152d2e
noobarafat <kmarafatislam@gmail.com>
2025-02-25 11:15:33 +0600

    CachedNetworkImaged

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,19 +14,24 @@ class LabClass05 extends StatelessWidget {
       title: 'Basic Flutter UI-02',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic Flutter UI-02"),
+          title: const Text("Basic Flutter UI-02"),
           backgroundColor: const Color.fromARGB(255, 21, 59, 224),
           centerTitle: true,
         ),
-        body: Row(
-          children: [
-            Image.network(
-              "https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg"
-            ), 
-            Image.network(
-              "https://images.pexels.com/photos/1366957/pexels-photo-1366957.jpeg"
-            ),
-          ],
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal, // Allows horizontal scrolling
+          child: Row(
+            children: [
+              CachedNetworkImage(
+                imageUrl: 
+                "https://www.citypng.com/public/uploads/preview/hd-blue-apple-watch-series-6-png-704081694622134gll0hgaum4.png",
+                placeholder:(context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => 
+                Text("Error Loading Image"),
+              )
+
+            ],
+          ),
         ),
       ),
     );
